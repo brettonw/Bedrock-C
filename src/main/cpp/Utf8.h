f9e86dfcbdb93529ce81311bf6465e181281c1ec
brettonw <bretton.wade@gmail.com>
2018-07-06 21:04:53 +0000

    Utf-8 decoder, refactor and testing

--- src/main/cpp/Utf8.h
+++ src/main/cpp/Utf8.h
@@ -2,102 +2,105 @@
 
 #pragma once
 
-class Utf8 {
-    private:
-        const char* position;
-        const char* next;
-        int codePoint;
-
-        // this UTF-8 decoding is modelled after the javascript at:
-        //     https://github.com/mathiasbynens/utf8.js/blob/master/utf8.js
-        // see https://encoding.spec.whatwg.org/#utf-8 for the UTF-8 specification
-        int readContinuationByte () {
-            if (*next) {
-                byte continuationByte = *next++;
-                if ((continuationByte & 0xC0) == 0x80) {
-                    return continuationByte & 0x3F;
-                }
-            }
-            // error case
-            throw runtime_error ("Invalid continuation byte");
-        }
-
-        void readNext () {
-            try {
-                uint byte1;
-                uint byte2;
-                uint byte3;
-                uint byte4;
-                if (*next == 0) {
-                    codePoint = 0;
-                    return;
-                }
-
-                // Read first byte
-                byte1 = *next++;
-
-                // 1-byte sequence (no continuation bytes)
-                if ((byte1 & 0x80) == 0) {
-                    codePoint = byte1;
-                    return;
-                }
-
-                // 2-byte sequence
-                if ((byte1 & 0xE0) == 0xC0) {
-                    byte2 = readContinuationByte();
-                    codePoint = ((byte1 & 0x1F) << 6) | byte2;
-                    if (codePoint >= 0x80) {
-                        return;
-                    } else {
-                        throw runtime_error("Invalid continuation byte");
-                    }
-                }
-
-                // 3-byte sequence (may include unpaired surrogates)
-                if ((byte1 & 0xF0) == 0xE0) {
-                    byte2 = readContinuationByte();
-                    byte3 = readContinuationByte();
-                    codePoint = ((byte1 & 0x0F) << 12) | (byte2 << 6) | byte3;
-                    if (codePoint >= 0x0800) {
-                        if (codePoint >= 0xD800 && codePoint <= 0xDFFF) {
-                            //throw runtime_error("Lone surrogate U+" + codePoint.toString(16).toUpperCase() + " is not a scalar value");
-                            throw runtime_error("Lone surrogate U+xxxxx is not a scalar value");
-                        }
-                        return;
-                    } else {
-                        throw runtime_error("Invalid continuation byte");
-                    }
-                }
-
-                // 4-byte sequence
-                if ((byte1 & 0xF8) == 0xF0) {
-                    byte2 = readContinuationByte();
-                    byte3 = readContinuationByte();
-                    byte4 = readContinuationByte();
-                    codePoint = ((byte1 & 0x07) << 0x12) | (byte2 << 0x0C) | (byte3 << 0x06) | byte4;
-                    if ((codePoint >= 0x010000) && (codePoint <= 0x10FFFF)) {
-                        return;
-                    }
-                }
-
-                throw runtime_error("Invalid UTF-8 detected");
-            } catch (...) {
-                codePoint = -1;
-            }
-        }
+// this UTF-8 handling is modeled after the javascript at: https://github.com/mathiasbynens/utf8.js
+// for the UTF-8 specification, see: https://encoding.spec.whatwg.org/#utf-8
+class Utf8Decoder {
     public:
-        Utf8 (const char* _position) : position (_position), next (_position) {
-            readNext ();
+        enum {
+            END_OF_BUFFER = 0,
+            UNREAD_BUFFER = 0xfffe,
+            ERROR = 0xffff
+        };
+
+    private:
+        const byte* position;
+        const byte* next;
+        uint codePoint;
+
+        uint readContinuationByte () {
+            if (*next) {
+                uint byteN = *next++;
+                if ((byteN & 0xc0) == 0x80) {
+                    return byteN & 0x3f;
+                }
+            }
+
+            // error case - hit the end of the buffer in the middle of reading a code point
+            throw runtime_error("UTF-8: unexpected END_OF_BUFFER");
         }
 
-        Utf8& operator ++ () {
+    public:
+        // the buffer must be null terminated...
+        Utf8Decoder (const void* buffer) : position (0), next (static_cast<const byte*> (buffer)), codePoint (UNREAD_BUFFER) {
+        }
+
+        bool goNext () {
             position = next;
-            readNext ();
-            return *this;
+            try {
+                // if we are at the beginning of a read sequence, and encounter the END_OF_BUFFER,
+                // then we will just stop
+                if (*next == END_OF_BUFFER) {
+                    codePoint = END_OF_BUFFER;
+                } else {
+                    // Read the first byte, and decide how to proceed based on that
+                    uint byte1 = *next++;
+                    if ((byte1 & 0x80) == 0) {
+                        // 1-byte sequence (0x00 - 0x7f)
+                        codePoint = byte1;
+                    } else if ((byte1 & 0xe0) == 0xc0) {
+                        // 2-byte sequence (0xc2 - 0xdf)
+                        uint byte2 = readContinuationByte();
+                        codePoint = ((byte1 & 0x1f) << 6) | byte2;
+                        if (codePoint < 0x80) {
+                            throw runtime_error("UTF-8: invalid continuation");
+                        }
+                    } else if ((byte1 & 0xf0) == 0xe0) {
+                        // 3-byte sequence (0xe0 - 0xef)
+                        uint byte2 = readContinuationByte();
+                        uint byte3 = readContinuationByte();
+                        codePoint = ((byte1 & 0x0f) << 12) | (byte2 << 6) | byte3;
+                        if (codePoint >= 0x0800) {
+                            if (codePoint >= 0xd800 and codePoint <= 0xdfff) {
+                                ostringstream out;
+                                out << "UTF-8: lone surrogate U+" << hex << codePoint << " is not a scalar value";
+                                throw runtime_error(out.str());
+                            }
+                        } else {
+                            throw runtime_error("UTF-8: invalid continuation");
+                        }
+                    } else if ((byte1 & 0xf8) == 0xf0) {
+                        // 4-byte sequence (0xf0 - 0xf4)
+                        uint byte2 = readContinuationByte();
+                        uint byte3 = readContinuationByte();
+                        uint byte4 = readContinuationByte();
+                        codePoint = ((byte1 & 0x07) << 18) | (byte2 << 12) | (byte3 << 6) | byte4;
+                        if ((codePoint < 0x010000) or (codePoint > 0x10ffff)) {
+                            throw runtime_error("UTF8: invalid continuation");
+                        }
+                    } else {
+                        throw runtime_error("UTF-8: invalid read");
+                    }
+                }
+            } catch (runtime_error& err) {
+                // set the error condition, and reset so that we will consistently hit that error
+                // condition if we keep trying to advance
+                cerr << err.what () << endl;
+                codePoint = ERROR;
+                next = position;
+            }
+            return ((codePoint != END_OF_BUFFER) and (codePoint != ERROR));
         }
 
-        int operator * () {
+        void skip (void) {
+            // function to skip ahead in the buffer to find the next valid code point
+        }
+
+        uint operator * () {
             return codePoint;
+        }
+
+        const byte* getCurrent () {
+            return position;
         }
 };
 
--- src/test/cpp/TestUtf8.cpp
+++ src/test/cpp/TestUtf8.cpp
@@ -1,12 +1,58 @@
 #include "Test.h"
 #include "Utf8.h"
 
-TEST_CASE(TestUtf8) {
+// utf-8 test file: https://www.w3.org/2001/06/utf-8-wrong/UTF-8-test.html
+// https://en.wikipedia.org/wiki/UTF-8
+
+TEST_CASE(TestUtf8Simple) {
     const char* str = "Hello World";
-    Utf8    reader (str);
-    while (*reader != 0) {
-        int compare = *str++;
-        TEST_XY(*reader, compare);
-        ++reader;
+    Utf8Decoder decoder (str);
+    while (decoder.goNext ()) {
+        uint compare = (uint) *str++;
+        TEST_XY(*decoder, compare);
     }
+    TEST_XY(decoder.goNext (), false);
 }
+
+// useful test resource: https://www.utf8-chartable.de/unicode-utf8-table.pl?start=12288&number=128
+TEST_CASE(TestUtf8Japanese) {
+    // Japanese characters are 3-byte sequences
+    // 5 chars - A, I, U, E, O
+    byte str[] = { 0xe3, 0x81, 0x82, 0xe3, 0x81, 0x84, 0xe3, 0x81, 0x86, 0xe3, 0x81, 0x88, 0xe3, 0x81, 0x8a, 0 };
+    uint compareTo[] = { 0x3042, 0x3044, 0x3046, 0x3048, 0x304A, 0 };
+    uint* xxx = &compareTo[0];
+    Utf8Decoder decoder (str);
+    while (decoder.goNext ()) {
+        uint compare = *xxx++;
+        TEST_XY(*decoder, compare);
+    }
+    TEST_XY(decoder.goNext (), false);
+}
+
+TEST_CASE(TestUtf8Mixed) {
+    // 5 chars - A, I, U, E, O - interspersed with "Hello"
+    byte str[] = { 0xe3, 0x81, 0x82, 'h', 0xe3, 0x81, 0x84, 'e', 0xe3, 0x81, 0x86, 'l', 0xe3, 0x81, 0x88, 'l', 0xe3, 0x81, 0x8a, 'o', 0 };
+    uint compareTo[] = { 0x3042, 'h', 0x3044, 'e', 0x3046, 'l', 0x3048, 'l', 0x304A, 'o', 0 };
+    uint* xxx = &compareTo[0];
+    Utf8Decoder decoder (str);
+    while (decoder.goNext ()) {
+        uint compare = *xxx++;
+        TEST_XY(*decoder, compare);
+    }
+    TEST_XY(decoder.goNext (), false);
+}
+
+TEST_CASE(TestUtf8Wiki) {
+    // 4 chars from the wiki page
+    byte str[] = { 0x24, 0xc2, 0xa2, 0xe2, 0x82, 0xac, 0xf0, 0x90, 0x8d, 0x88, 0 };
+    uint compareTo[] = { 0x0024, 0x00a2, 0x20ac, 0x10348, 0 };
+    uint* xxx = &compareTo[0];
+    Utf8Decoder decoder (str);
+    while (decoder.goNext ()) {
+        uint compare = *xxx++;
+        TEST_XY(*decoder, compare);
+    }
+    TEST_XY(decoder.goNext (), false);
+}
+
+// need 2-byte seuences and 4-byte sequences
