345cdd4e0bff67905deccfa81360470ee86b834a
Bretton Wade <brettonw.wade@gmail.com>
2018-07-20 14:13:36 -0400

    tweaks

--- source/common/Log.h
+++ source/common/Log.h
@@ -7,8 +7,8 @@ class Log {
         enum {
             TRACE = 0,
             DEBUG = 1,
-            INFO = 2,
-            WARN = 3,
+            INFO  = 2,
+            WARN  = 3,
             ERROR = 4
         };
 
