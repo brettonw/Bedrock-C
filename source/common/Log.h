aa428f1cd6a8a9b88ff8b7861285b0ba6b3b7f47
Bretton Wade <bretton.wade@gmail.com>
2018-07-20 12:36:47 -0400

    logging and log testing

--- source/common/Log.h
+++ source/common/Log.h
@@ -11,6 +11,7 @@ class Log {
             WARN = 3,
             ERROR = 4
         };
+
         class Level {
             friend class Log;
             private:
@@ -22,31 +23,34 @@ class Log {
         };
 
     private:
-        ostream& stream;
+        ostream* stream;
         uint filterLevel;
         uint level;
 
-        Log (ostream& _stream, uint _filterLevel = INFO, uint _level = INFO) : stream (_stream), filterLevel (_filterLevel), level (_level) {
-            // XXX TODO: figure out how to load these settings from a file...
+        Log (ostream& _stream, uint _filterLevel = INFO, uint _level = INFO) : stream (&_stream), filterLevel (_filterLevel), level (_level) {
         }
 
+        static Log* log;
         static Log& singleton () {
-            static Log log (cerr);
-            return log;
+            if (log == 0) {
+            // XXX TODO: load the default settings from a file...
+                log = new Log (cerr);
+            }
+            return *log;
         }
 
     public:
         template <typename InputType>
         Log& operator << (const InputType& input) {
             if (level >= filterLevel) {
-                stream << input;
+                (*stream) << input;
             }
             return *this;
         }
 
         Log& operator << (std::ostream&(*f)(std::ostream&)) {
             if (level >= filterLevel) {
-                stream << f;
+                (*stream) << f;
             }
             return *this;
         }
@@ -56,12 +60,18 @@ class Log {
             return *this;
         }
 
-        Log& filter (Level& _filterLevel) {
-            filterLevel = _filterLevel.level;
+        Log& setStream (std::ostream& _stream) {
+            stream = &_stream;
+            return *this;
+        }
+
+        Log& setFilterLevel (uint _filterLevel) {
+            filterLevel = _filterLevel;
             return *this;
         }
 
         // global static accessors
+        static Log& base () { return singleton (); }
         static Log& trace () { return singleton () << Level (TRACE); }
         static Log& debug () { return singleton () << Level (DEBUG); }
         static Log& info () { return singleton () << Level (INFO); }
--- source/test-common/Log.cpp
+++ source/test-common/Log.cpp
@@ -1,6 +1,85 @@
 #include        "Test.h"
 #include        "Log.h"
 
-TEST_CASE (TestLog) {
-    Log::info() << "Hello World" << endl;
+TEST_CASE (TestLogFilterTrace) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::TRACE);
+
+    Log::trace() << "trace" << endl;
+    Log::debug() << "debug" << endl;
+    Log::info() << "info" << endl;
+    Log::warn() << "warn" << endl;
+    Log::error() << "error" << endl;
+
+    TEST_XY(Text (out.str ().c_str ()), Text("trace\ndebug\ninfo\nwarn\nerror\n"));
+}
+
+TEST_CASE (TestLogFilterDebug) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::DEBUG);
+
+    Log::trace() << "trace" << endl;
+    Log::debug() << "debug" << endl;
+    Log::info() << "info" << endl;
+    Log::warn() << "warn" << endl;
+    Log::error() << "error" << endl;
+
+    TEST_XY(Text (out.str ().c_str ()), Text("debug\ninfo\nwarn\nerror\n"));
+}
+
+TEST_CASE (TestLogFilterInfo) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::INFO);
+
+    Log::trace() << "trace" << endl;
+    Log::debug() << "debug" << endl;
+    Log::info() << "info" << endl;
+    Log::warn() << "warn" << endl;
+    Log::error() << "error" << endl;
+
+    TEST_XY(Text (out.str ().c_str ()), Text("info\nwarn\nerror\n"));
+}
+
+TEST_CASE (TestLogFilterWarn) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::WARN);
+
+    Log::trace() << "trace" << endl;
+    Log::debug() << "debug" << endl;
+    Log::info() << "info" << endl;
+    Log::warn() << "warn" << endl;
+    Log::error() << "error" << endl;
+
+    TEST_XY(Text (out.str ().c_str ()), Text("warn\nerror\n"));
+}
+
+TEST_CASE (TestLogFilterError) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::ERROR);
+
+    Log::trace() << "trace" << endl;
+    Log::debug() << "debug" << endl;
+    Log::info() << "info" << endl;
+    Log::warn() << "warn" << endl;
+    Log::error() << "error" << endl;
+
+    TEST_XY(Text (out.str ().c_str ()), Text("error\n"));
+}
+
+TEST_CASE (TestLogMulti) {
+    ostringstream out;
+
+    Log::base().setStream (out).setFilterLevel (Log::INFO)
+        << Log::Level (Log::TRACE) << "trace"
+        << Log::Level (Log::DEBUG) << "debug"
+        << Log::Level (Log::INFO) << "info"
+        << Log::Level (Log::WARN) << "warn"
+        << Log::Level (Log::ERROR) << "error";
+
+    TEST_XY(Text (out.str ().c_str ()), Text("infowarnerror"));
 }
