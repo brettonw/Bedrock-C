96183b2d83fa8c97ee3e4e80772de95c445c92bd
Bretton Wade <bretton.wade@gmail.com>
2018-07-23 00:54:42 -0400

    millisecond test, with different pyrotechnics

--- source/test-common/Pause.cpp
+++ source/test-common/Pause.cpp
@@ -27,3 +27,30 @@ TEST_CASE(TestMicroPause) {
     testMicro (300);
     testMicro (500);
 }
+
+void testMilli (int milliseconds) {
+    int average = 0;
+    int sampleCount = 10;
+    for (int i = 0; i < sampleCount; ++i) {
+        auto startTime = chrono::high_resolution_clock::now();
+        Pause::milli (milliseconds);
+        auto stopTime = chrono::high_resolution_clock::now();
+        int delta = static_cast<int> (chrono::duration_cast<chrono::milliseconds>(stopTime - startTime).count ());
+        average += delta;
+    }
+    average /= sampleCount;
+    Log::debug () << "testMicro: " << "requested " << milliseconds << " ms pause, waited for " << average << " ms on average over " << sampleCount << " pauses" << endl;
+    // ensure it is within 3ms or 5%, whichever is greater
+    TEST_XYF(average, milliseconds, max (milliseconds / 20, 3));
+}
+
+TEST_CASE(TestMilliPause) {
+    Log::Scope  scope (Log::DEBUG);
+    testMilli (10);
+    testMilli (20);
+    testMilli (50);
+    testMilli (100);
+    testMilli (200);
+}
+
+
