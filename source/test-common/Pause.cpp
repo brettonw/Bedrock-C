ce5d6dbbc84fd53b61d1db6014129c1571ad505e
Bretton Wade <bretton.wade@gmail.com>
2018-07-23 00:43:34 -0400

    average the pauses.

--- source/test-common/Pause.cpp
+++ source/test-common/Pause.cpp
@@ -2,13 +2,19 @@
 #include "Pause.h"
 
 void testMicro (int microseconds) {
-    auto startTime = chrono::high_resolution_clock::now();
-    Pause::micro (microseconds);
-    auto stopTime = chrono::high_resolution_clock::now();
-    int delta = static_cast<int> (chrono::duration_cast<chrono::microseconds>(stopTime - startTime).count ());
-    Log::debug () << "testMicro: " << "requested " << microseconds << " us pause, waited for " << delta << " us" << endl;
+    int average = 0;
+    int sampleCount = 10;
+    for (int i = 0; i < sampleCount; ++i) {
+        auto startTime = chrono::high_resolution_clock::now();
+        Pause::micro (microseconds);
+        auto stopTime = chrono::high_resolution_clock::now();
+        int delta = static_cast<int> (chrono::duration_cast<chrono::microseconds>(stopTime - startTime).count ());
+        average += delta;
+    }
+    average /= sampleCount;
+    Log::debug () << "testMicro: " << "requested " << microseconds << " us pause, waited for " << average << " us on average over " << sampleCount << " pauses" << endl;
     // ensure it is within 5% error
-    //TEST_XYF(delta, microseconds, microseconds / 20);
+    TEST_XYF(average, microseconds, microseconds / 20);
 }
 
 TEST_CASE(TestMicroPause) {
