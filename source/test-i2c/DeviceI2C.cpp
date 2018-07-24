53b53533f49e3b92d0de7cbecf43eae30adef1dc
Bretton Wade <brettonw.wade@gmail.com>
2018-07-24 01:38:39 -0400

    update the device test, trim the output from the stepper test

--- source/test-i2c/DeviceI2C.cpp
+++ source/test-i2c/DeviceI2C.cpp
@@ -4,6 +4,7 @@
 
 TEST_CASE(TestDeviceI2C) {
     try {
+        Log::Scope scope (Log::TRACE);
         DeviceI2C   device (ADAFRUIT_MOTOR_DRIVER_DEFAULT_ADDRESS);
 
         device.write (0x43, 0x10);
