728b41c8737dad0e9e0d5ee374c8c154e5523223
Bretton Wade <brettonw.wade@gmail.com>
2018-07-24 07:15:44 -0400

    comment update

--- source/test-i2c/DeviceI2C.cpp
+++ source/test-i2c/DeviceI2C.cpp
@@ -7,9 +7,11 @@ TEST_CASE(TestDeviceI2C) {
         Log::Scope scope (Log::TRACE);
         DeviceI2C   device (ADAFRUIT_MOTOR_DRIVER_DEFAULT_ADDRESS);
 
-        // 0x44 is a high LED OFF register - so it can be set to a range of values. if you try to
-        // set the ON register - it will fail (silently) if the value is greater than the
-        // corresponding off register
+        // this test checks whether the device is active and responding by writing a register and
+        // confirming that value. The register, 0x44 is a high LED OFF register on the adafruit
+        // motor driver - so it can be set to a range of values. if you try to set the ON register,
+        // it will fail (silently) if the value is greater than the corresponding off register.
+        // XXX TODO: find a general way to do this check across all devices
         device.write (0x44, 0x10);
         Pause::milli (10);
         int x = device.read (0x44);
