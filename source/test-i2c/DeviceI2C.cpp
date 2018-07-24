b304bec0aaa5da0fd52a629bf89b64f6959daf9c
Bretton Wade <brettonw.wade@gmail.com>
2018-07-24 02:03:08 -0400

    try reading and writing an oof register instead of the on register.

--- source/test-i2c/DeviceI2C.cpp
+++ source/test-i2c/DeviceI2C.cpp
@@ -7,21 +7,21 @@ TEST_CASE(TestDeviceI2C) {
         Log::Scope scope (Log::TRACE);
         DeviceI2C   device (ADAFRUIT_MOTOR_DRIVER_DEFAULT_ADDRESS);
 
-        device.write (0x43, 0x10);
+        device.write (0x44, 0x10);
         Pause::milli (10);
-        int x = device.read (0x43);
+        int x = device.read (0x44);
         TEST_XY(x, 0x10);
         Pause::milli (10);
 
-        device.write (0x43, 0x11);
+        device.write (0x44, 0x11);
         Pause::milli (10);
-        x = device.read (0x43);
+        x = device.read (0x44);
         TEST_XY(x, 0x11);
         Pause::milli (10);
 
-        device.write (0x43, 0x20);
+        device.write (0x44, 0x20);
         Pause::milli (10);
-        x = device.read (0x43);
+        x = device.read (0x44);
         TEST_XY(x, 0x20);
 
         Pause::milli (1000);
