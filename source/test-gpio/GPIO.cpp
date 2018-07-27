c2854ce402c823eda049fc9f8b3116030bf892d3
Bretton Wade <brettonw.wade@gmail.com>
2018-07-27 17:45:16 -0400

    a little bit of simplification

--- source/test-gpio/GPIO.cpp
+++ source/test-gpio/GPIO.cpp
@@ -6,6 +6,7 @@ TEST_CASE(TestGPIO) {
     try {
         GPIO    gpio;
 
+        /*
         vector<Text>    names;
         names.push_back ("INPUT ");
         names.push_back ("OUTPUT");
@@ -15,22 +16,26 @@ TEST_CASE(TestGPIO) {
         names.push_back ("ALT_1 ");
         names.push_back ("ALT_2 ");
         names.push_back ("ALT_3 ");
+        */
+        const char* functions[] = {
+            "INPUT", "OUTPUT", "ALT_5 ", "ALT_4 ", "ALT_0 ", "ALT_1 ", "ALT_2 ", "ALT_3 "
+        };
 
         GPIO::Function function;
         for (uint piPin = 0; piPin < RASPBERRY_PI_PIN_COUNT;) {
             Log& log = Log::debug () << "TestGPIO: " << ((piPin < 10) ? " " : "");
             try {
                 function = gpio.getFunction (static_cast<Pin> (getPin (static_cast<PiPin> (piPin))));
-                log << (piPin + 1) << " -> " << names[function] << " | ";
+                log << (piPin + 1) << " -> " << functions[function] << " | ";
             } catch (RuntimeError& runtimeError) {
-                log << (piPin + 1) << " -> " << "  XX  " << " | ";
+                log << (piPin + 1) << " -> " << "     *" << " | ";
             }
             ++piPin;
             try {
                 function = gpio.getFunction (static_cast<Pin> (getPin (static_cast<PiPin> (piPin))));
-                log << names[function] << " <- " << (piPin + 1) << endl;
+                log << functions[function] << " <- " << (piPin + 1) << endl;
             } catch (RuntimeError& runtimeError) {
-                log << "  XX  " << " <- " << (piPin + 1) << endl;
+                log << "*     " << " <- " << (piPin + 1) << endl;
             }
             ++piPin;
         }
