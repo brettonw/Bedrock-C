15a4d80fd0539b584d5b74aa0de3010e54f03cf9
Bretton Wade <brettonw.wade@gmail.com>
2018-07-27 13:32:40 -0400

    GPIO output cleanup

--- source/test-gpio/GPIO.cpp
+++ source/test-gpio/GPIO.cpp
@@ -7,18 +7,23 @@ TEST_CASE(TestGPIO) {
         GPIO    gpio;
 
         vector<Text>    names;
-        names.push_back ("INPUT");
+        names.push_back ("INPUT ");
         names.push_back ("OUTPUT");
-        names.push_back ("ALTERNATE_5");
-        names.push_back ("ALTERNATE_4");
-        names.push_back ("ALTERNATE_0");
-        names.push_back ("ALTERNATE_1");
-        names.push_back ("ALTERNATE_2");
-        names.push_back ("ALTERNATE_3");
+        names.push_back ("ALT_5 ");
+        names.push_back ("ALT_4 ");
+        names.push_back ("ALT_0 ");
+        names.push_back ("ALT_1 ");
+        names.push_back ("ALT_2 ");
+        names.push_back ("ALT_3 ");
 
-        for (uint pin = 0; pin < GPIO_PIN_COUNT; ++pin) {
+        for (uint pin = 0; pin < GPIO_PIN_COUNT;) {
             GPIO::Function function = gpio.getFunction (static_cast<Pin> (pin));
-            Log::debug () << "TestGPIO: " << "pin " << pin << " is " << names[function] << endl;
+            //Log::debug () << "TestGPIO: " << "pin " << pin << " is " << names[function] << endl;
+            Log& log = Log::debug () << "TestGPIO: " << pin << " -> " << function << " | ";
+            ++pin;
+            function = gpio.getFunction (static_cast<Pin> (pin + 1));
+            log << function << " <- " << pin << endl;
+            ++pin;
         }
 
     } catch (RuntimeError& runtimeError) {
