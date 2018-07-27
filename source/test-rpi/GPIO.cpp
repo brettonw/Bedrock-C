a74edbf1bc9b5aebecae778e4b7769b906a63938
Bretton Wade <brettonw.wade@gmail.com>
2018-07-26 23:23:47 -0400

    names

--- source/test-rpi/GPIO.cpp
+++ source/test-rpi/GPIO.cpp
@@ -6,34 +6,19 @@ TEST_CASE(TestGPIO) {
     try {
         GPIO    gpio;
 
+        vector<Text>    names;
+        names.push_back ("INPUT");
+        names.push_back ("OUTPUT");
+        names.push_back ("ALTERNATE_0");
+        names.push_back ("ALTERNATE_1");
+        names.push_back ("ALTERNATE_2");
+        names.push_back ("ALTERNATE_3");
+        names.push_back ("ALTERNATE_4");
+        names.push_back ("ALTERNATE_5");
+
         for (uint pin = 0; pin < GPIO::PIN_COUNT; ++pin) {
             GPIO::Function function = gpio.getFunction (static_cast<GPIO::Pin> (pin));
-            switch (function) {
-                case GPIO::Function::INPUT:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "INPUT" << endl;
-                break;
-                case GPIO::Function::OUTPUT:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "OUTPUT" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_0:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_0" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_1:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_1" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_2:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_2" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_3:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_3" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_4:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_4" << endl;
-                break;
-                case GPIO::Function::ALTERNATE_5:
-                    Log::debug () << "TestGPIO: " << "pin " << pin << " is " << "ALTERNATE_5" << endl;
-                break;
-            }
+            Log::debug () << "TestGPIO: " << "pin " << pin << " is " << names[function] << endl;
         }
 
     } catch (RuntimeError& runtimeError) {
