99438d32f1ff8c6b11757fcef0b7c707f2c88a4d
brettonw <bretton.wade@gmail.com>
2018-08-01 17:22:57 +0000

    tweaks

--- source/bag/BagContainer.h
+++ source/bag/BagContainer.h
@@ -9,10 +9,11 @@ MAKE_PTR_TO_SUB(BagContainer, BagThing) {
     public:
         BagContainer (Type _type) : BagThing (_type) {}
 
+        virtual double sortValue () const { return 0.0; };
+
         PtrToBagThing get (const Text& path) {
             return const_cast<const BagContainer*>(this)->get (path);
         }
 
         virtual const PtrToBagThing get (const Text& path) const = 0;
-        virtual double sortValue () const { return 0.0; };
 };
