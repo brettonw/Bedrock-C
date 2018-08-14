269639f31c2c4a5af18f68dc4dd31fd6735b5e7e
Bretton Wade <bretton.wade@gmail.com>
2018-08-13 20:12:17 -0400

    math

--- source/common/Math.h
+++ source/common/Math.h
@@ -1,8 +1,25 @@
 #pragma once
 
+/*
 template <typename NumberType>
 int signum(NumberType value) {
     return (NumberType(0) < value) - (value < NumberType(0));
+}
+*/
+
+template <typename NumberType> inline constexpr
+int signum (T value, false_type is_signed) {
+    return (T(0) < value);
+}
+
+template <typename NumberType> inline constexpr
+int signum (NumberType value, true_type is_signed) {
+    return (NumberType (0) < value) - (value < NumberType (0));
+}
+
+template <typename NumberType> inline constexpr
+int signum (NumberType value) {
+    return signum(value, is_signed<NumberType>());
 }
 
 template <typename NumberType>
