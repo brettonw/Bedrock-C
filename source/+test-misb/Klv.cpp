f5504b2e9f2bb3b0fbe4c744974f20f30405bbd8
brettonw <bretton.wade@gmail.com>
2018-10-05 21:16:01 +0000

    checkpoint

--- source/+test-misb/Klv.cpp
+++ source/+test-misb/Klv.cpp
@@ -10,7 +10,7 @@ TEST_CASE(KlvDictionary) {
     Text keyText ("06.0e.2b.34.01.01.01.01.0e.01.01.03.2d.02.00.00");
     UniversalLabelKey key (keyText);
 
-    Klv klv (File ("stennis.klv").read());
+    Klv klv (Buffer::make (0));
     PtrToBagObject entry = klv.getKeyDescription(key);
     Text name = entry->getText("Name");
     TEST_EQUALS (name, "Sigma_Height");
