0eec95af49431bcb99481037536acbaf26775868
brettonw <bretton.wade@gmail.com>
2018-10-11 21:38:10 +0000

    checkpoint on revised KLV handling architecture

--- source/+test-misb/Klv.cpp
+++ source/+test-misb/Klv.cpp
@@ -4,18 +4,6 @@
 
 TEST_MODULE_DEPENDENCIES (Klv, "UniversalLabelKey,MisbByteStreamReader,MisbByteStreamWriter")
 
-TEST_CASE(KlvDictionary) {
-    //Log::Scope scope (Log::INFO);
-
-    Text keyText ("06.0e.2b.34.01.01.01.01.0e.01.01.03.2d.02.00.00");
-    UniversalLabelKey key (keyText);
-
-    Klv klv (Buffer::make (0));
-    PtrToBagObject entry = klv.getKeyDescription(key);
-    Text name = entry->getText("Name");
-    TEST_EQUALS (name, "Sigma_Height");
-}
-
 TEST_CASE(KlvRead) {
     Log::Scope scope (Log::DEBUG);
 
