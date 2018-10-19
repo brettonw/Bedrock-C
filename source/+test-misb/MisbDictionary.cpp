9394e223ae7477968013e27036a6005e5f14b3be
brettonw <bretton.wade@gmail.com>
2018-10-19 19:36:21 +0000

    checkpoint back to an array based misb database, constructed to hash map on load

--- source/+test-misb/MisbDictionary.cpp
+++ source/+test-misb/MisbDictionary.cpp
@@ -9,15 +9,8 @@ TEST_CASE(MisbDictionary) {
     Text keyText ("06.0e.2b.34.01.01.01.01.0e.01.01.03.2d.02.00.00");
     UniversalLabelKey key (keyText);
 
-    MisbDictionary dictionary (MISB_DIR "misb-dictionary.json");
+    MisbDictionary dictionary (MISB_DIR "misb-database.json");
     PtrToBagObject entry = dictionary.get(key);
     Text name = entry->getText("Name");
     TEST_EQUALS (name, "Sigma_Height");
 }
-
-/*
-TEST_CASE(Rewrite) {
-    MisbDictionary dictionary (MISB_DIR "misb-dictionary.json");
-    dictionary.rewrite(MISB_DIR "misb-database.json");
-}
-*/
