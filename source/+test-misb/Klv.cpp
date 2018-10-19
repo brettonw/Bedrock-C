c2f6697e61ff65bd4607914faf6be2a463c7d3c8
brettonw <bretton.wade@gmail.com>
2018-10-19 21:19:42 +0000

    comment update

--- source/+test-misb/Klv.cpp
+++ source/+test-misb/Klv.cpp
@@ -32,8 +32,8 @@ TEST_CASE(ST0902) {
     // this test file contains two UAS datalink packets given in ST0902.7, section 9, Annex C
     // the standard contains an omission in data for tag 0x14 where it sets the bytes to the value
     // 00 00 00 00. The checksum given is correct for this case, but the example shows a different
-    // value of 7D C5 5E CE. When changed in the packet, this causes the checksum to be incorrect.
-    // we corrected the checksums in our data file.
+    // value of 7D C5 5E CE. When changed it in the packet, this caused the checksum to be
+    // incorrect. we corrected the checksums in our data file.
     File st0902 ("klv/0902.hex");
     Text hex0902 = st0902.readText();
     vector<Text> hexBytes = hex0902.split (" ");
