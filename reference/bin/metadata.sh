6e9805cfdd552f62637c24a02b87cc51c4cb3b89
brettonw <bretton.wade@gmail.com>
2018-10-26 20:45:54 +0000

    MISB test files - just the klv and the pdfs

--- reference/bin/metadata.sh
+++ reference/bin/metadata.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 input=$1;
-klvoutput=${input%.*}.klv
-hexoutput=${input%.*}.hex
-ffmpeg -i $input -map data-re -codec copy -f data $klvoutput
-hexdump -C $klvoutput > $hexoutput
+#echo "Processing $input";
+klvoutput=${input%.*}.klv;
+hexoutput=${input%.*}.hex;
+ffmpeg -i $input -map data-re -codec copy -f data $klvoutput;
+hexdump -C $klvoutput > $hexoutput;
 
