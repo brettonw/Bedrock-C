e5b056ee1f41c0631ff74df8b6340b698e016b24
Bretton Wade <brettonw.wade@gmail.com>
2018-10-30 22:57:48 -0400

    dictionary loads from url!

--- source/+test-misb/Klv.cpp
+++ source/+test-misb/Klv.cpp
@@ -13,7 +13,7 @@ TEST_CASE(KlvRead) {
         if ((*iter)->getBasename() == "SD_MP2_0601_SYNC") {
             Log::debug () << "Reading " << (*iter)->getBasename() << endl;
             if ((*iter)->getExtension() == "klv") {
-                PtrToMisbDictionary misbDictionary = new MisbDictionary (MISB_DIR "misb-database.json");
+                PtrToMisbDictionary misbDictionary = MisbDictionary::fromUrl ();
                 Klv (misbDictionary, (*iter)->read()).readUniversalSetAll ();
             }
         }
@@ -36,7 +36,7 @@ TEST_CASE(ST0902) {
     for (vector<Text>::iterator iter = hexBytes.begin (); iter != hexBytes.end (); ++iter) {
         bytes.push_back(fromHex (*iter));
     }
-    PtrToMisbDictionary misbDictionary = new MisbDictionary (MISB_DIR "misb-database.json");
+    PtrToMisbDictionary misbDictionary = MisbDictionary::fromUrl ();
     Klv (misbDictionary, Buffer::make (bytes.data(), bytes.size())).readUniversalSet ();
     TEST_TRUE(true);
 }
--- source/+test-misb/MisbDictionary.cpp
+++ source/+test-misb/MisbDictionary.cpp
@@ -9,8 +9,8 @@ TEST_CASE(MisbDictionary) {
     Text keyText ("06.0e.2b.34.01.01.01.01.0e.01.01.03.2d.02.00.00");
     UniversalLabelKey key (keyText);
 
-    MisbDictionary dictionary (MISB_DIR "misb-database.json");
-    PtrToBagObject entry = dictionary.get(key);
+    PtrToMisbDictionary dictionary = MisbDictionary::fromFile ();
+    PtrToBagObject entry = dictionary->get(key);
     Text name = entry->getText("Name");
     TEST_EQUALS (name, "Sigma_Height");
 }
