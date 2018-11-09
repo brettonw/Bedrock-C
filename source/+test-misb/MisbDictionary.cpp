6b2310071b5c1ccca7e6f2fcca866fea6f0b0628
brettonw <bretton.wade@gmail.com>
2018-11-09 20:46:15 +0000

    database update, some formatting

--- source/+test-misb/MisbDictionary.cpp
+++ source/+test-misb/MisbDictionary.cpp
@@ -14,3 +14,39 @@ TEST_CASE(MisbDictionary) {
     Text name = entry->getText("Name");
     TEST_EQUALS (name, "Sigma_Height");
 }
+
+void addToStandard (PtrToBagObject& standardsObject, const Text& standard, PtrToBagObject& entry) {
+    if (not standardsObject->contains(standard)) {
+        standardsObject->put (standard, PtrToBagArray (new BagArray ()));
+    }
+    standardsObject->getBagArray (standard)->add(entry);
+}
+
+TEST_CASE(WriteMisbStandard) {
+    Log::Scope scope (Log::DEBUG);
+    PtrToFile databaseFile = new File (MISB_DATABASE_NAME);
+    if (databaseFile->getExists ()) {
+        PtrToBagThing database = Json::readFile(databaseFile);
+        if (database) {
+            PtrToBagArray array = ptr_downcast<BagArray> (database);
+            if (array) {
+                PtrToBagObject standardsObject (new BagObject ());
+                Log::debug() << "Reading MISB database: " << databaseFile->getBasename() << ", with " << array->size() << " entries." << endl;
+                for (uint i = 0, end = array->size(); i < end; ++i) {
+                    PtrToBagObject entry = array->getBagObject(i);
+                    Text standard = entry->contains ("Standard") ? entry->getText("Standard") : "XXX";
+                    standard = standard.replace("/", "-");
+                    addToStandard (standardsObject, standard, entry);
+                }
+
+                // now loop over the keys of the standards object and report the numbers
+                vector<Text> keys = standardsObject->keys();
+                for (vector<Text>::const_iterator iter = keys.begin (); iter != keys.end (); ++iter) {
+                    PtrToBagArray array = standardsObject->getBagArray (*iter);
+                    Log::debug() << "  Standard (" << *iter << ") with " << array->size() << " entr" << ((array->size() == 1) ? "y" : "ies") << endl;
+                }
+            }
+        }
+    }
+    TEST_TRUE (true);
+}
