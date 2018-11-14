8cfb8ef2d6d3746cb971943cf93843d69f4131da
brettonw <bretton.wade@gmail.com>
2018-11-14 21:20:33 +0000

    checkpoint on splitting up the KLV database

--- source/+test-misb/Klv.cpp
+++ source/+test-misb/Klv.cpp
@@ -7,10 +7,10 @@ TEST_MODULE_DEPENDENCIES (Klv, "UniversalLabelKey,MisbByteStreamReader,MisbByteS
 TEST_CASE(KlvRead) {
     Log::Scope scope (Log::DEBUG);
 
-    File klvDirectory ("klv");
+    File klvDirectory ("klv/misb/0601.4");
     vector<PtrToFile> files = klvDirectory.getFiles();
     for (vector<PtrToFile>::iterator iter = files.begin (); iter != files.end (); ++iter) {
-        if ((*iter)->getBasename() == "HD_H264_0903_TS_SYN_V1_001") {
+        if ((*iter)->getBasename() != "xxx") {
             Log::debug () << "Reading " << (*iter)->getBasename() << endl;
             if ((*iter)->getExtension() == "klv") {
                 PtrToMisbDictionary misbDictionary = MisbDictionary::fromFiles ();
@@ -22,21 +22,21 @@ TEST_CASE(KlvRead) {
 }
 
 TEST_CASE(ST0902) {
-    //Log::Scope scope (Log::DEBUG);
+    Log::Scope scope (Log::DEBUG);
 
     // this test file contains two UAS datalink packets given in ST0902.7, section 9, Annex C
     // the standard contains an omission in data for tag 0x14 where it sets the bytes to the value
     // 00 00 00 00. The checksum given is correct for this case, but the example shows a different
     // value of 7D C5 5E CE. When changed it in the packet, this caused the checksum to be
     // incorrect. we corrected the checksums in our data file.
-    File st0902 ("klv/0902.hex");
+    File st0902 ("klv/misb/0601.6/0902.hex");
     Text hex0902 = st0902.readText();
     vector<Text> hexBytes = hex0902.split (" ");
     vector<byte> bytes;
     for (vector<Text>::iterator iter = hexBytes.begin (); iter != hexBytes.end (); ++iter) {
         bytes.push_back(fromHex (*iter));
     }
-    PtrToMisbDictionary misbDictionary = MisbDictionary::fromFiles ();
+    PtrToMisbDictionary misbDictionary = MisbDictionary::fromFiles ({ "SMPTE-RP-210.13", "MISB-ST-0601.6", "MISB-ST-0102.10", "MISB-ST-0806.4", "MISB-ST-0301.5", "MISB-ST-0801.5", "MISB-RP-1204.0"});
     Klv (misbDictionary, Buffer::make (bytes.data(), bytes.size())).readUniversalSet ();
     TEST_TRUE(true);
 }
