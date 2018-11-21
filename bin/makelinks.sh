ea2f753fb393f6a5b7f0769b54e94aa4e12e5adc
Bretton Wade <brettonw.wade@gmail.com>
2018-11-21 00:24:20 -0500

    adding the makelinks function

---
+++ bin/makelinks.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+ln -nsf ../../Bedrock-C/source/common common
+ln -nsf ../../Bedrock-C/source/bag bag
+ln -nsf ../../Bedrock-C/source/text text
+ln -nsf ../../Bedrock-C/source/test test
+ln -nsf ../../Bedrock-C/source/+test-common +test-common
+ln -nsf ../../Bedrock-C/source/+test-bag +test-bag
+ln -nsf ../../Bedrock-C/source/+test-text +test-text
