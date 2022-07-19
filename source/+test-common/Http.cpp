e69d2df2ec8153ce79c4ac883aec11c5bdb9c5af
Bretton Wade <bretton.wade@gmail.com>
2022-07-18 23:22:48 -0400

    master - test update

--- source/+test-common/Http.cpp
+++ source/+test-common/Http.cpp
@@ -5,6 +5,6 @@ TEST_MODULE_DEPENDENCIES (Http, "File")
 
 TEST_CASE(HttpGet) {
     Text result = Http::getText ("https://bedrock.brettonw.com/api?event=ok", new File ("result.json"));
-    TEST_EQUALS(result, "{\"query\":{\"event\":\"ok\"},\"status\":\"ok\"}\n");
+    TEST_NOT_EQUALS(result.find ("\"query\":{\"event\":\"ok\"}"), -1);
 }
 
